6cd5d82e6c37c32fda2309b122cf86178ec8259f
Qiugu He <kaochi545121151@gmail.com>
2019-05-29 13:40:29 -0500

    Final vision

--- src/StackAndQueue.java
+++ src/StackAndQueue.java
@@ -3,71 +3,79 @@ public class StackAndQueue {
 
 	public static void main(String [] args)
 	{
-		Queue q = new Queue();
-		Stack s = new Stack();
-		
-		System.out.println("Please enter an string: ");
-		Scanner scan = new Scanner(System.in);
-		while(scan.hasNextLine()) {
+		while(true) {
+			// get user input
+			System.out.println("Please enter a string: ");
+			Scanner scan = new Scanner(System.in);
 			String input = scan.nextLine().replace("\n","");
+			if(input.equals("")) {
+				scan.close();
+				break;
+			}
+			//remove all special character
 			input = input.replaceAll( "[^a-zA-Z0-9 ]" , "" );
 			input = input.replaceAll( "\\s+" , "" );
 			
-			if(input.equals("")) {
-				break;
-			}
+			//initialize stack and queue 
+			Queue q = new Queue();
+			Stack s = new Stack();
 			char []charArray = input.toCharArray();
+			//store all chars into stack and queue
 			for(int i = 0; i < charArray.length; i++)
 			{
 				s.push(charArray[i]);
 				q.enqueue(charArray[i]);
 			}
 			
+			//for test purpose
 			/*System.out.println(s.toString());
 			System.out.println(q.toString());*/
 			
 			Boolean check = false;
+			//check if the string is palindrome
 			while(!s.isEmpty())
 			{	
 				if(Character.toLowerCase(s.pop()) != Character.toLowerCase(q.dequeue()))
 				{	
 					check = true;
-					System.out.println("The String is not palindrome.");
-					reset(s, q);
+					System.out.println("The String is not palindrome.\n");
 					break;
 				}
 			}
 			if(!check) {
-				System.out.println("The String is palindrome.");
+				System.out.println("The String is palindrome.\n");
 			}
-			System.out.println("\nPlease enter an string: ");
-			scan = new Scanner(System.in);
-			
 	  }//input
-		scan.close();
-		System.out.println("Program end normaly.");
+		System.out.println("Program end normally.");
 	}//main
-	
-	public static void reset(Stack s, Queue q){
-		s.sreset();
-		q.qreset();
-	}
-}
+}// end class
 
+//-----------------
+// Node class
+//-----------------
 class Node{
-	char data;
-	Node next;
+	char data; // the item to be stored
+	Node next; // next link
+	
+	//constructor
 	public Node(char data, Node next) {
 		this.data= data;
 		this.next = next;
 	}
 }
 
+//********************
+// Queue class
+//*******************
 class Queue{
 	Node front, tail;
+	
+	//constructor
 	public Queue() {
 		this.front = this.tail = null;
 	}
+	
+	//enqueue
 	public void enqueue(char x)
 	{
 		Node newNode = new Node(x, null);
@@ -78,6 +86,8 @@ class Queue{
 		this.tail.next = newNode;
 		this.tail = newNode;
 	}
+	
+	//dequeue
 	public char dequeue() {
 		if(this.front == null) {
 			return 0;
@@ -91,6 +101,7 @@ class Queue{
 		return temp.data;
 	}
 	
+	//toString
 	public String toString()
 	{
 		String toReturn = "";
@@ -102,30 +113,34 @@ class Queue{
 		}
 		return toReturn;
 	}
-	public void qreset()
-	{
-		front = tail = null;
-	}
-	
-}
+}//end Queue
 
+//-----------------
+// Stack class
+//----------------
 class Stack{
 	Node top;
+	//constructor
 	public Stack() {
 		this.top = null;
 	}
+	
+	//push
 	public void push(char x)
 	{
 		Node newNode = new Node(x, null);
 		newNode.next = top;
 		top = newNode;
 	}
+	
+	//isEmpty
 	public boolean isEmpty()
 	{
 		return top == null;
 	}
-	public char pop()
-	{
+	
+	//pop
+	public char pop(){
 		if(!isEmpty())
 		{
 			Node temp = top;
@@ -138,6 +153,8 @@ class Stack{
 			return 0;
 		}
 	}
+	
+	//peek
 	public void peek() {
 		if (top == null)
 		{
@@ -146,6 +163,8 @@ class Stack{
 		}
 		top = top.next;
 	}
+	
+	//toString
 	public String toString()
 	{
 		String toReturn = "";
@@ -157,8 +176,4 @@ class Stack{
 		}
 		return toReturn;
 	}
-	public void sreset()
-	{	
-		top = null;
-	}
-}
+}//end Stack
